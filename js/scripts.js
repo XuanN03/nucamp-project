4ae686350a9c67e7d69fde6cd8bf52814c13b3da
Xuan Nguyen <xtnguyen03@gmail.com>
2025-02-01 13:07:16 -0500

    weather update

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,32 @@ carouselButton.addEventListener('click', function(){
 //     console.log('cycle the carousel');
 //     carousel.cycle();
 // })
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = 'Ridgeway';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    const res = await fetch(url);
+    const result = await res.json();
+
+    console.log(result);
+    displayWeather(result);
+    
+}
+
+fetchWeather();
+
+function displayWeather(result) {
+    const icon = document.createElement('img');
+    const iconData = result.weather[0].icon;
+    const temp = result.main.temp;
+    const desc = result.weather[0].description;
+
+    icon.src = `https://openweathermap.org/img/w/${iconData}.png`;
+    document.querySelector('#weather-icon').appendChild(icon);
+
+    document.querySelector('#weather-temp').textContent = temp;
+    document.querySelector('#weather-description').textContent = desc;
+
+}
